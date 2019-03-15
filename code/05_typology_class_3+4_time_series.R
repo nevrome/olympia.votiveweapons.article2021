8415bc28d74e46fa7000f70d9d175eea1060bc3b
Clemens Schmid <clemens@nevrome.de>
2019-03-15 14:33:48 +0100

    started work on time series joyplot to increase usefulness and usability

--- code/05_typology_class_3+4_time_series.R
+++ code/05_typology_class_3+4_time_series.R
@@ -4,32 +4,26 @@ library(ggplot2)
 load("data/weapons.RData")
 artefacts <- weapons %>%
   dplyr::mutate(
-    typology_fine = ifelse(!is.na(typology_class_4), paste0(typology_class_2, "+", typology_class_4), typology_class_2)
+    typology_fine = ifelse(!is.na(typology_class_4), paste0(typology_class_3, "+", typology_class_4), typology_class_3)
   )
 
 artefact_timeseries_df <- aoristAAR::aorist(
   artefacts,
-  split_vars = c("typology_class_2", "typology_fine"),
+  split_vars = c("typology_class_3", "typology_fine"),
   from = "dating_typology_start",
   to = "dating_typology_end"
 )
 
-artefact_timeseries_df_roll_avg <- artefact_timeseries_df %>%
+artefact_timeseries_df %<>% dplyr::filter(
+  sum != 0
+)
+
+artefact_timeseries_df <- artefact_timeseries_df %>%
   dplyr::group_by(
     typology_fine
   ) %>%
   dplyr::arrange(
     date
-  ) %>%
-  dplyr::mutate(
-    roll_sum = zoo::rollapply(
-      data = sum, 
-      width = 10, 
-      FUN = mean, 
-      align = "right", 
-      fill = NA, 
-      na.rm = T
-    )
   ) %>%
   dplyr::mutate(
     total_sum = sum(sum, na.rm = T),
@@ -42,7 +36,7 @@ artefact_timeseries_df_roll_avg <- artefact_timeseries_df %>%
     -total_sum, -cum_sum, -fit
   )
 
-typology_fine_centers <- artefact_timeseries_df_roll_avg %>%
+typology_fine_centers <- artefact_timeseries_df %>%
   dplyr::filter(
     center
   )
@@ -59,7 +53,7 @@ typology_fine_levels <- typology_fine_centers %>%
   typology_fine %>%
   as.character()
 
-artefact_timeseries <- artefact_timeseries_df_roll_avg %>%
+artefact_timeseries <- artefact_timeseries_df %>%
   dplyr::mutate(
     typology_fine = factor(typology_fine, levels = typology_fine_levels)
   )
@@ -67,24 +61,25 @@ artefact_timeseries <- artefact_timeseries_df_roll_avg %>%
 p <- ggplot() +
   ggridges::geom_density_ridges(
     data = artefact_timeseries,
-    mapping = aes(x = date, y = typology_fine, height = roll_sum, fill = typology_class_2),
+    mapping = aes(x = date, y = typology_fine, height = sum, fill = typology_class_3),
     stat = "identity",
     alpha = 0.5
   ) +
   geom_point(
     data = typology_fine_centers,
-    mapping = aes(x = date, y = typology_fine, color = typology_class_2)
+    mapping = aes(x = date, y = typology_fine, color = typology_class_3)
   ) +
   geom_point(
     data = typology_fine_centers,
-    mapping = aes(x = -1200, y = typology_fine, color = typology_class_2),
+    mapping = aes(x = -800, y = typology_fine, color = typology_class_3),
     size = 3
   ) +
   theme_bw() +
   theme(
     axis.text.x = element_text(angle = 25, hjust = 1),
     legend.position = "bottom"
-  )
+  ) +
+  xlim(-800, -400)
 
 ggsave(
   filename = "05_typology_class_3+4_time_series.png",
