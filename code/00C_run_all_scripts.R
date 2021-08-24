3e66ba2672e1311bde2a554f39ee4118090f9e79
Clemens Schmid <clemens@nevrome.de>
2021-08-24 15:39:01 +0200

    small adjustments and renaming

--- code/00C_run_all_scripts.R
+++ code/00C_run_all_scripts.R
@@ -1,5 +1,5 @@
 code_files_raw <- list.files("code", full.names = TRUE)
-code_files <- code_files_raw[!grepl("00A|00B|00C", code_files_raw)]
+code_files <- code_files_raw[!grepl("00A|00B|00C|00D", code_files_raw)]
 
 purrr::walk(
   code_files,
